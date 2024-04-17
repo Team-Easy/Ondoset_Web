ab309ad6971c20ef4e8c28a0a2a2ec822cf5e54a
FpRaArNkK <mspark1184@naver.com>
2024-04-18 03:13:15 +0900

    Feat: add Dashboard

--- ondoset-admin/src/App.js
+++ ondoset-admin/src/App.js
@@ -10,8 +10,8 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 
@@ -34,8 +34,8 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
